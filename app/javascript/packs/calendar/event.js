63c8a7c85a10fcc4adc8d5002bdebee840c85832
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-04 10:34:05 +0900

    予定表示

--- app/javascript/packs/calendar/event.js
+++ app/javascript/packs/calendar/event.js
@@ -12,6 +12,8 @@ document.addEventListener('turbolinks:load', function() {
     var calendar = new Calendar(calendarEl, {
         plugins: [ monthGridPlugin, interactionPlugin, googleCalendarApi ],
 
+        events: '/events.json', // <=これを追加
+        // 書き方のルールとしては['/コントローラー名.json']としてください
 
         //細かな表示設定
         locale: 'ja',
@@ -38,10 +40,59 @@ document.addEventListener('turbolinks:load', function() {
         },
         eventClassNames: function(arg){
             //表示されたイベントにclassをcss用に追加する(詳しくは次回の記事へ)
-        }
+        },
+
+//     });
+//     //カレンダー表示
+//     calendar.render();
+
+// });
+
+        //---ここから---//
+        //日付をクリックした時に発生させるイベント
+        dateClick: function(info){
+            //クリックした日付の情報を取得
+            const year  = info.date.getFullYear();
+            const month = (info.date.getMonth() + 1);
+            const day   = info.date.getDate();
+
+            //ajaxでevents/newを着火させ、htmlを受け取ります
+            $.ajax({
+                type: 'GET',
+                url:  '/events/new',
+            }).done(function (res) {
+                // 成功処理
+                // 受け取ったhtmlをさっき追加したmodalのbodyの中に挿入します
+                $('.modal-body').html(res);
+
+                //フォームの年、月、日を自動入力
+                $('#event_start_1i').val(year);
+                $('#event_start_2i').val(month);
+                $('#event_start_3i').val(day);
+
+                $('#event_end_1i').val(year);
+                $('#event_end_2i').val(month);
+                $('#event_end_3i').val(day);
+
+                //ここのidはevents/newのurlにアクセスするとhtmlがコードとして表示されるので、
+                //開始時間と終了時間のフォームを表しているところのidを確認してもらうことが確実です
+
+                $('#modal').fadeIn();
+
+            }).fail(function (result) {
+                // 失敗処理
+                alert("failed");
+            });
+        },
 
     });
-    //カレンダー表示
+
     calendar.render();
 
+    //この下からも追加
+    //成功、失敗modalを閉じたときに予定を再更新してくれます
+    //これがないと追加しても自動更新されません
+    $(".error").click(function(){
+        calendar.refetchEvents();
+    });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'events/index'
   root 'events#index'
+  resources :events
 end
